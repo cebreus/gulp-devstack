75e57cd1e84e807cce96c5cbe7ed7c35576ad8d9
Emil Čelustka <emil.celustka@gmail.com>
2019-06-30 15:53:25 +0000

    feat: function with sri hashes

--- gulpfile-build.js
+++ gulpfile-build.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const compileSassFnc = require('./gulp-tasks-build/gulp-compile-sass');
 const concatFilesFnc = require('./gulp-tasks-build/gulp-concat-files');
 const revisionFnc = require('./gulp-tasks-build/gulp-revision');
+const replaceHashFnc = require('./gulp-tasks-build/gulp-sri-hash');
 
 // Variables
 // --------------
@@ -33,12 +34,17 @@ function revision() {
     return revisionFnc(params);
 }
 
+function replaceHash() {
+    return replaceHashFnc(`${config.buildBase}/*.html`, config.buildBase);
+}
+
 // Gulp tasks
 // --------------
 
 gulp.task('build:css', gulp.parallel(compileSassAll));
 gulp.task('revision', gulp.series(revision));
+gulp.task('replace-hash', replaceHash);
 
 // Aliases
 
-gulp.task('default', gulp.series('build:css'));
+gulp.task('default', gulp.series('build:css', revision, replaceHash));
