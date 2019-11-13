7f53171c4ad5c8e1472ef1428e72722c9e97ca54
Emil Čelustka <emil.celustka@gmail.com>
2019-11-13 15:53:20 +0000

    feat: build tasks

--- gulpconfig-build.js
+++ gulpconfig-build.js
@@ -7,7 +7,7 @@ const flexbugsFixes = require('postcss-flexbugs-fixes');
 
 const devBase = './src';
 const buildBase = './build';
-const templateFolder = './temp';
+const tempBase = './temp';
 
 // SASS
 // --------------
@@ -22,12 +22,42 @@ const sassCustom = [
 ];
 const sassCore = [`${sassBase}/bootstrap.scss`, `${sassBase}/_variables.scss`];
 const sassUtils = [`${sassBase}/u-*.scss`, `${sassBase}/_variables.scss`];
+const injectCss = `${sassBuild}/*.css`;
+// Data JSON
+// --------------
+
+const datasetJsonBase = `${devBase}/data/**/*.json`;
+const datasetJsonFileName = 'dataset.json';
+const datasetJsonBuild = tempBase;
+
+// Templates
+// --------------
+
+const tplBase = `${devBase}/pages`;
+// const tplMain = `${tplBase}/**/*.html`;
+const tplMain = `${tplBase}/index.html`;
+const tplBuild = `${buildBase}`;
+const tplDataset = `${tempBase}/dataset.json`;
 
 // GFX
 // --------------
 
 const gfxBase = `${devBase}/gfx`;
 const gfxBuild = `${buildBase}/images`;
+
+// JavaScript
+// --------------
+
+const jsBase = `${devBase}/js`;
+const jsFiles = `${jsBase}/*.js`;
+const jsBuild = `${buildBase}/js`;
+const injectJs = `${jsBuild}/*.js`;
+
+const injectCdnJs = [
+  '<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>',
+  '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>',
+  '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>'
+];
 
 const svgBase = `${gfxBase}/svg`;
 const svgImages = `${svgBase}/*.svg`;
@@ -52,16 +82,28 @@ const postcssPluginsBase = [
 module.exports = {
   devBase: devBase,
   buildBase: buildBase,
-  templateFolder: templateFolder,
+  tempBase: tempBase,
   sassBase: sassBase,
   sassBuild: sassBuild,
   sassAll: sassAll,
   sassCustom: sassCustom,
   sassCore: sassCore,
   sassUtils: sassUtils,
+  injectCss: injectCss,
   gfxBase: gfxBase,
   gfxBuild: gfxBuild,
+  datasetJsonBase: datasetJsonBase,
+  datasetJsonBuild: datasetJsonBuild,
+  datasetJsonFileName: datasetJsonFileName,
+  tplBase: tplBase,
+  tplMain: tplMain,
+  tplBuild: tplBuild,
+  tplDataset: tplDataset,
+  injectCdnJs: injectCdnJs,
   postcssPluginsBase: postcssPluginsBase,
+  jsFiles: jsFiles,
+  jsBuild: jsBuild,
+  injectJs: injectJs,
   gfxBuild: gfxBuild,
   svgImages: svgImages,
   jpgImages: jpgImages,
