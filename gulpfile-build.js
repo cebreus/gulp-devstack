5fdfd3d36dd57f8494b2f73d8476c7d2fcd57558
Emil Čelustka <emil.celustka@gmail.com>
2019-11-13 15:53:20 +0000

    feat: build tasks

--- gulpfile-build.js
+++ gulpfile-build.js
@@ -3,9 +3,11 @@ const gulp = require('gulp');
 const cleanBuildFnc = require('./gulp-tasks-build/gulp-clean-build');
 const compileSassFnc = require('./gulp-tasks-build/gulp-compile-sass');
 const concatFilesFnc = require('./gulp-tasks-build/gulp-concat-files');
+const buildHtmlFnc = require('./gulp-tasks-build/gulp-build-html');
 const revisionFnc = require('./gulp-tasks-build/gulp-revision');
 const replaceHashFnc = require('./gulp-tasks-build/gulp-sri-hash');
 
+const buildDatasetFnc = require('./gulp-tasks/gulp-build-dataset');
 const imagesFnc = require('./gulp-tasks/gulp-optimize-images');
 
 // Variables
@@ -29,20 +31,50 @@ function compileSassAll() {
   );
 }
 
+function buildDataset(done) {
+  buildDatasetFnc(
+    config.datasetJsonBase,
+    config.datasetJsonBuild,
+    config.datasetJsonFileName,
+    () => {
+      done();
+    }
+  );
+}
+
+function buildHtml(done) {
+  const params = {
+    input: config.tplMain,
+    output: config.tplBuild,
+    dataSource: config.tplDataset,
+    injectCdnJs: config.injectCdnJs,
+    injectCss: config.injectCss,
+    injectJs: config.injectJs,
+    cb: () => {
+      done();
+    }
+  };
+  buildHtmlFnc(params);
+}
+
 function revision() {
   const params = {
     inputRevision: `${config.buildBase}/**/*.css`,
     outputRevision: config.buildBase,
-    ouputManifest: `${config.templateFolder}/revision`,
+    ouputManifest: `${config.tempBase}/revision`,
     inputRewrite: `${config.buildBase}/*.html`,
     outputRewrite: config.buildBase,
-    manifestFile: `${config.templateFolder}/revision/*.json`
+    manifestFile: `${config.tempBase}/revision/*.json`
   };
   return revisionFnc(params);
 }
 
 function replaceHash() {
   return replaceHashFnc(`${config.buildBase}/*.html`, config.buildBase);
+}
+
+function concatFiles() {
+  return concatFilesFnc(config.jsFiles, config.jsBuild, 'index.min.js');
 }
 
 function images(done) {
@@ -64,4 +96,16 @@ gulp.task('images', images);
 
 // Aliases
 
-gulp.task('default', gulp.series('build:css', revision, replaceHash));
+gulp.task(
+  'default',
+  gulp.series(
+    cleanBuild,
+    'build:css',
+    concatFiles,
+    buildDataset,
+    revision,
+    buildHtml,
+    replaceHash,
+    images
+  )
+);
