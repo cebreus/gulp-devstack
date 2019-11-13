b52a38395bf4eafa001b968ec8e8eedf46d50673
Emil Čelustka <emil.celustka@gmail.com>
2019-11-13 16:33:07 +0000

    feat: function addRevision

--- gulp-tasks-build/gulp-revision.js
+++ gulp-tasks-build/gulp-revision.js
@@ -4,6 +4,17 @@ const revRewrite = require('gulp-rev-rewrite');
 const revReplace = require('gulp-rev-replace');
 const revDelete = require('gulp-rev-delete-original');
 
+/**
+ * @description Add version hash to files
+ * @param {string} inputRevision path to CSS files
+ * @param {string} outputRevision path to save files
+ * @param {string} outPutManifest path to save manifest file
+ * @param {string} inputRewrite path to HTML files which you want to rewrite
+ * @param {string} manifestFile path to manifest file
+ * @param {string} outputRewrite path to save rewrited HTML files
+ * @return {stream} Files with version hash
+ */
+
 const revision = (params) => {
   return gulp
     .src(params.inputRevision)
