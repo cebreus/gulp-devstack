9d0b8bbe474e670e1c2b35983b1ad7a333ca0a44
Jaroslav Vrána <cebreus@live.com>
2020-04-15 22:50:38 +0200

    fix: unify versions of the JavaScript libraries

--- gulpconfig-build.js
+++ gulpconfig-build.js
@@ -67,8 +67,8 @@ const injectJs = `${jsBuild}/*.js`;
 
 const injectCdnJs = [
   '<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>',
-  '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>',
-  '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>',
+  '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>',
+  '<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>',
 ];
 
 // Modules & Plugins
